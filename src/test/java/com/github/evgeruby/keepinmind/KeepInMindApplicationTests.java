7a07488b564c66c9e14dc67be6062d5cfe6fb521
Evgeniya Rubanova <e.rubanova@gmail.com>
2021-09-13 15:25:14 +0300

    specifies structure

---
+++ src/test/java/com/github/evgeruby/keepinmind/KeepInMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.evgeruby.keepinmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepInMindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
